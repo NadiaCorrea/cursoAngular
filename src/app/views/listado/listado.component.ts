4bf13ea7220ecf6472a6ba5fd80ecdfdf6cafb1b
NadiaCorrea <ncormor536@gmail.com>
2023-01-04 21:44:24 +0100

    creating routes and data-binding

--- src/app/views/listado/listado.component.ts
+++ src/app/views/listado/listado.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Entrada } from '../../shared/interfaces/entrada';
 
 @Component({
   selector: 'app-listado',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listado.component.css']
 })
 export class ListadoComponent implements OnInit {
+  public listadoEntradas:Entrada[];
 
-  constructor() { }
+
+  constructor() {
+    this.listadoEntradas = [
+      {titulo:'Introducción',
+      resumen: 'Inicio del curso'},
+      {titulo:'Tema 1',
+      resumen: 'Tema 1: Typescript'},
+      {titulo:'Tema 2',
+      resumen: 'Angular'}
+    ];
+   }
 
   ngOnInit(): void {
   }
 
+  public mostrarTitulo(titulo:string):void{
+    alert(`Entrada seleccionada: ${titulo}`);
+  }
 }
